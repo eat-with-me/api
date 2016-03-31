8719166f7366c390abb9ffded6437a4185ad4c86
Bartosz Erbert <erbert.b@gmail.com>
2016-03-31 17:16:30 +0200

    seeds for groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+include FactoryGirl::Syntax::Methods
+
+create(:group_with_orders)
