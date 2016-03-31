9e7e7a610037654a5140152f1a0001d63502e081
Bartosz Erbert <erbert.b@gmail.com>
2016-03-31 20:33:45 +0200

    creating orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,22 @@
 class OrdersController < ApplicationController
   def index
-    render :json => Group.find(offer_index_param).orders.to_json(
+    render :json => Group.find(offer_group_param).orders.to_json(
       include: :restaurant
     )
   end
 
-  def offer_index_param
+  def create
+    order = Group.find(offer_group_param)
+      .orders
+      .create(offer_create_param)
+    render :json => order
+  end
+
+  def offer_group_param
     params.permit(:group_id)["group_id"]
+  end
+
+  def offer_create_param
+    params.permit(:restaurant_id, :closing_time)
   end
 end
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -11,4 +11,23 @@ RSpec.describe OrdersController, type: :controller do
       expect(output.length).to be(5)
     end
   end
+
+  describe 'POST #create' do
+    before(:example) do
+      group = create(:group_with_orders)
+      restaurant = create(:restaurant)
+      @order = {
+        group_id: group[:id],
+        restaurant_id: restaurant[:id],
+        closing_time: "12:00"
+      }
+    end
+
+    it 'returns http success' do
+      post :create, @order
+      output = JSON.parse(response.body)
+
+      puts output
+    end
+  end
 end
